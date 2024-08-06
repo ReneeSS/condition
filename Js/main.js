c299cb638113eeb5c079dd14af9e6b39a55fbec8
ReneeSS <reneeffs@gmail.com>
2024-08-06 17:55:09 +0300

    switch and case

--- Js/main.js
+++ Js/main.js
@@ -5,3 +5,30 @@ if (4 == 9){
 } else {
   console.log("Error!");
 }
+
+const num = 99;
+/*
+if (num < 49){
+  console.log("It is true")
+} else {
+  console.log("try again")
+}
+
+// Тернарный оператор
+
+(num === 50) ? console.log("ok!") : console.log("Error!");
+*/
+switch (num) {
+  case 49:
+    console.log("It isn't true");
+    break;
+  case 100:
+    console.log("It isn't true");
+    break;
+  case 50:
+    console.log("you are right");
+    break;
+  default:
+    console.log("It isn't true");
+    break;
+}
