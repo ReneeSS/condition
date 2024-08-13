9af2ac5e3e2c7caf6e98af6b30689b73e090d30f
ReneeSS <reneeffs@gmail.com>
2024-08-13 18:31:35 +0300

    full lesson

--- Js/main.js
+++ Js/main.js
@@ -32,3 +32,45 @@ switch (num) {
     console.log("It isn't true");
     break;
 }
+
+
+// logical operators
+
+
+const hamburger = 3;
+const fries = 3;
+const cola = 0;
+const nuggets = 2;
+
+console.log(hamburger === 3 && cola === 2 || fries === 3 && nuggets);
+
+if (hamburger === 3 && cola === 2 || fries === 3 && nuggets){
+  console.log("We are happy")
+} else {
+  console.log("It's not good")
+};
+
+console.log(hamburger === 3 && cola === 2);
+
+if (hamburger === 3 && cola === 1 && fries){
+  console.log("Я сыт!")
+} else {
+  console.log("I want to eat")
+}
+
+const hamburgerNew = 5;
+const friesNew = 0;
+
+if (hamburgerNew === 3 && friesNew){
+  console.log("Я сыт!")
+} else {
+  console.log("We are leaving from this cafe")
+}
+
+
+let johnReport, alexReport, samReport, mariaReport = "done";
+
+console.log(johnReport || alexReport || samReport || mariaReport)
+
+
+console.log(!1);
